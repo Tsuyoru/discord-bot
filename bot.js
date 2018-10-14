a9b37355a9ab8e1fa03423d740c5d5f00426264c
Tsuyoru <44127187+Tsuyoru@users.noreply.github.com>
2018-10-14 09:44:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,8 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-//var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -37,3 +29,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
          }
      }
 });
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
