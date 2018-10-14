d89d825d6f02be0c21556c1a1f204e37f6040a55
Tsuyoru <44127187+Tsuyoru@users.noreply.github.com>
2018-10-14 09:33:07 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.'NTAwODA0ODQzMzc2NzM4MzIy.DqQpow.r7PwjbIsG2GtebyXBmsnubZoJ9M');
+client.login(process.env.BOT_TOKEN);
